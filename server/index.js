18ebc5c58a8c72c0ff5f5307ca52646fb2ce385d
ManiacAyu <Ayush Dhoke>
2024-11-07 10:35:17 +0530

    commit3269

--- server/index.js
+++ server/index.js
@@ -5,7 +5,19 @@ const connectDB = require("./config/connectDB");
 const router = require("./routes/index");
 const cookiesParser = require("cookie-parser");
 const { app, server } = require("./socket/index");
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://chat-app-git-main-maniacayus-projects.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  // Handle preflight requests for complex CORS requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 // const app = express()
 
 console.log(process.env.FRONTEND_URL);
